f251d0d6dae6e57fa67c21244435140c91cbad0f
johana gitari <johanagitari@gmail.com>
2023-02-20 08:47:50 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is the entry
+ *Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
