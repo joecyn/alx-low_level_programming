ab008854484d4bfff39a195c46fb2cf9ef59eb78
johana gitari <johanagitari@gmail.com>
2023-02-20 08:38:28 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 /**
   *main-main entry point
   *Return: 1 if the number is positive. 0 otherwise
-  */`
+  */
 int main(void)
 {
-	char msg[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(msg);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
