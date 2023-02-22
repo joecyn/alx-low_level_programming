0d5eb657ded34c4c33ea02169df82384e71ce96d
johana gitari <johanagitari@gmail.com>
2023-02-22 10:31:41 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Check if a number is greater than 0
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	fputs("_putchar\n");
+	return (0);
+}
+
